dd8a16d7857a12c1f10abe58d9a1ad13bd596588
Enrique Dapelo <enricodapelo6@hotmail.com>
2022-11-02 17:41:57 +0100

    done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -11,6 +11,38 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get("/create", (req, res) => {
+    res.render("characters/create-character")
+})
+
+/* router.get("/characters/:id/update", (req, res) => {
+    res.render(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+}) */
+
+router.post("/characters/create", (req, res, next) => {
+    console.log(req.body)
+    axios.post('https://ih-crud-api.herokuapp.com/characters', req.body)
+    .then(responseFromAPI => {
+        res.redirect("/characters")
+    })
+})
+
+router.get("/characters/:id/update", (req, res, next) => {
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        // console.log("details: ", responseFromAPI.data)
+        res.render("characters/edit-character", { character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+});
+
+router.post("/characters/:id/update", async (req, res, next) => {
+     await axios.put(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`, req.body)
+    .then(responseFromAPI => {
+        res.redirect(`/characters/${req.params.id}`)
+    })
+    .catch(err => console.error(err))
+})
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -20,6 +52,15 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+router.post("/characters/:id/delete", (req, res) => {
+    axios.delete(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    .then(responseFromAPI => {
+        res.redirect("/characters")
+    })
+})
+
+
 
 module.exports = router;
 
